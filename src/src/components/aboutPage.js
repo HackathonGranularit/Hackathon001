bf914e9762fde2e7d4c9f94181edd0ca9df59811
briancanspt@gmail.com <briancanspit@gmail.com>
2019-10-29 14:16:05 +0300

    Removed carousel, modified About page

--- src/src/components/aboutPage.js
+++ src/src/components/aboutPage.js
@@ -7,12 +7,42 @@ import SEO from "../components/seo"
 const AboutPage = () => (
   <Layout>
     <SEO title="About" />
-    <h1>About us</h1>
-    
-
-    <div>
-      <div cols>
-
+    <br />
+    <div className="row slide-in-fwd-center">
+      <div className="col col-md-6">
+        <h1 style={{ textAlign: "center" }}>Practice Profile</h1>
+        <p style={{ textAlign: "center" }}>
+          Tandem & Stark Ltd is an independent Kenyan firm that provides quality
+          Cost Engineering, Cost Consultancy Services and project/ construction
+          management in the construction industry. Our wealth of our experience
+          covers a wide variety of projects ranging from building and
+          engineering support, fitting out & maintenance works. Since its
+          establishment, the firm has been able to provide the above services to
+          clients across East and Central Africa in the Commercial and
+          Industrial Sector, Financial Institutions, Domestic Housing as well as
+          in the Hospitality and Leisure industry.
+        </p>
+      </div>
+      <div className="col col-md-6">
+        <h1>What We Offer</h1>
+        <div className="row">
+          <div className="col col-md-6">
+            <ul>
+              <li>Quality Construction</li>
+              <li>Affordable Service</li>
+              <li>Endorsed work</li>
+              <li>Approved Personnel</li>
+            </ul>
+          </div>
+          <div className="col col-md-6">
+            <ul>
+              <li>Quality Construction</li>
+              <li>Affordable Service</li>
+              <li>Endorsed Work</li>
+              <li>Approved Personnel</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
 
--- src/src/components/projectsPage.js
+++ src/src/components/projectsPage.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-import DemoCarousel from "./carouselReact"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,7 +7,6 @@ import SEO from "../components/seo"
 const ProjectsPage = () => (
   <Layout>
     <SEO title="Projects" />
-    <DemoCarousel />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
