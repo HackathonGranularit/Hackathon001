caa27313b8383d2101be1128a1bb79c09b3c9963
Ngatiajeffers <jeffersongakuya@gmail.com>
2019-10-29 12:39:15 +0300

    TeamA initial commit

--- src/src/pages/About.js
+++ src/src/pages/About.js
@@ -1,13 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import  "../components/aboutus"
 import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <div className="about-us-banner">
-      <div className="">
+      <div className="aboutus-banner">
         <img src="" alt="about-us-banner-image"/>
 
       </div>
